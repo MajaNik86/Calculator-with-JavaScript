41bcd5a5d5f15d449d912786071f75e05322232e
MajaNik86 <majanik86@gmail.com>
2023-03-06 10:51:57 +0100

    Started JS, added functionality for number btns to display and reset btn

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('hello')
+const calculatorDisplay = document.querySelector('h1');
+const inputBtns = document.querySelectorAll('button');
+const clearAllBtn = document.getElementById('clear-btn')
+
+
+//function for number values:
+function sendNumberValue(number) {
+    console.log(number)
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+
+}
+
+
+//Add event listeners to btn for numbers: 
+inputBtns.forEach((inputBtn) => {
+    if (inputBtn.classList.length === 0) {
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    }
+})
+
+
+//Clear all function:
+function clearAll() {
+    calculatorDisplay.textContent = '0';
+}
+
+clearAllBtn.addEventListener('click', clearAll);
